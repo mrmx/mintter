305a9675b89b445da6ecee54ccf79b399612289b
Horacio Herrera <hi@horacioh.com>
2023-11-13 13:14:09 +0100

    add update package properly

--- frontend/apps/desktop/src/update.ts
+++ frontend/apps/desktop/src/update.ts
@@ -1,9 +1,9 @@
+import updateApp from 'update-electron-app'
+
 export function setupUpdates() {
   // We delay this work by 1s to ensure that the
   // app doesn't have to worry about updating during launch
   setTimeout(() => {
-    const updateApp = require('update-electron-app')
-
     updateApp({
       repo: 'MintterHypermedia/mintter',
       updateInterval: '1 hour',
