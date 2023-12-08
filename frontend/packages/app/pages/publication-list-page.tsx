ebed8aca926d35f2c0411ad042b80a5406118af1
Eric Vicenti <eric@vicenti.net>
2023-12-08 14:46:53 -0800

    Clean up Publication List Virtualization
    # Conflicts:
    #	frontend/packages/app/pages/publication-list-page.tsx

--- frontend/packages/app/pages/publication-list-page.tsx
+++ frontend/packages/app/pages/publication-list-page.tsx
@@ -12,143 +12,94 @@ import {
   DialogTitle,
   SizableText,
   Spinner,
-  TamaguiElement,
-  View,
   XStack,
   YStack,
 } from '@mintter/ui'
-import {Virtuoso} from 'react-virtuoso'
 
 import {createPublicWebHmUrl, idToUrl, unpackHmId} from '@mintter/shared'
 import copyTextToClipboard from 'copy-text-to-clipboard'
-import {useCallback, useEffect, useRef, useState} from 'react'
+import {memo} from 'react'
 import {useAppContext} from '../app-context'
 import {DeleteDocumentDialog} from '../components/delete-dialog'
 import {useAppDialog} from '../components/dialog'
 import {copyLinkMenuItem} from '../components/list-item'
-import {MainWrapper, MainWrapperNoScroll} from '../components/main-wrapper'
+import {MainWrapper} from '../components/main-wrapper'
 import {PublicationListItem} from '../components/publication-list-item'
 import {queryPublication, usePublicationFullList} from '../models/documents'
 import {useWaitForPublication} from '../models/web-links'
 import {toast} from '../toast'
 
-export function PublicationListPage({
+export const PublicationListPage = memo(PublicationListPageUnmemo)
+
+export function PublicationListPageUnmemo({
   trustedOnly,
   empty,
 }: {
   trustedOnly: boolean
   empty?: React.ReactNode
 }) {
-  let publications = usePublicationFullList({trustedOnly})
-  let drafts = useDraftList()
-  let {queryClient, grpcClient} = useAppContext()
-  let openDraft = useOpenDraft('push')
+  const publications = usePublicationFullList({trustedOnly})
+  const drafts = useDraftList()
+  const {queryClient, grpcClient} = useAppContext()
+  const openDraft = useOpenDraft('push')
   const items = publications.data
-
   const deleteDialog = useAppDialog(DeleteDocumentDialog, {isAlert: true})
-  const container = useRef<TamaguiElement>(null)
-  const virtuoso = useRef(null)
-  const [dimensions, setDimensions] = useState({height: 0, width: 0})
-  const handleContainer = useCallback((node: TamaguiElement | null) => {
-    container.current = node
-    setDimensions({
-      height: node?.offsetHeight,
-      width: node?.offsetWidth,
-    })
-  }, [])
-  useEffect(() => {
-    function handleResize() {
-      const node = container.current
-      setDimensions({
-        height: node?.offsetHeight,
-        width: node?.offsetWidth,
-      })
-    }
-    window.addEventListener('resize', handleResize)
-    return () => {
-      window.removeEventListener('resize', handleResize)
-    }
-  }, [])
   if (items) {
     if (items.length) {
       return (
         <>
-          <MainWrapperNoScroll>
-            <YStack f={1} ref={handleContainer}>
-              <Virtuoso
-                ref={virtuoso}
-                style={{
-                  height: dimensions.height,
-                  display: 'flex',
-                  overflowY: 'scroll',
-                  overflowX: 'hidden',
-                }}
-                increaseViewportBy={{
-                  top: 800,
-                  bottom: 800,
-                }}
-                components={{
-                  Header: () => <View style={{height: 30}} />,
-                  Footer: () => <View style={{height: 30}} />,
-                }}
-                id="scroll-page-wrapper"
-                totalCount={items.length}
-                itemContent={(index) => {
-                  const {publication, author, editors} = items[index]
-                  const docId = publication.document?.id
-                  if (!docId) return null
-                  return (
-                    <XStack
-                      key={publication.document?.id}
-                      jc="center"
-                      width={dimensions.width}
-                    >
-                      <PublicationListItem
-                        pubContext={trustedOnly ? {key: 'trusted'} : null}
-                        openRoute={{
-                          key: 'publication',
-                          documentId: docId,
-                          pubContext: trustedOnly ? {key: 'trusted'} : null,
-                        }}
-                        hasDraft={drafts.data?.documents.find(
-                          (d) => d.id == publication.document?.id,
-                        )}
-                        onPointerEnter={() => {
-                          if (publication.document?.id) {
-                            queryClient.client.prefetchQuery(
-                              queryPublication(
-                                grpcClient,
-                                publication.document.id,
-                                publication.version,
-                              ),
-                            )
-                          }
-                        }}
-                        publication={publication}
-                        author={author}
-                        editors={editors}
-                        menuItems={[
-                          copyLinkMenuItem(
-                            idToUrl(docId, undefined, publication.version),
-                            'Publication',
+          <MainWrapper>
+            <Container>
+              {items.map((item) => {
+                const {publication, author, editors} = item
+                const docId = publication.document?.id
+                if (!docId) return null
+                return (
+                  <PublicationListItem
+                    key={docId}
+                    pubContext={trustedOnly ? {key: 'trusted'} : null}
+                    openRoute={{
+                      key: 'publication',
+                      documentId: docId,
+                      pubContext: trustedOnly ? {key: 'trusted'} : null,
+                    }}
+                    hasDraft={drafts.data?.documents.find(
+                      (d) => d.id == publication.document?.id,
+                    )}
+                    onPointerEnter={() => {
+                      if (publication.document?.id) {
+                        queryClient.client.prefetchQuery(
+                          queryPublication(
+                            grpcClient,
+                            publication.document.id,
+                            publication.version,
                           ),
-                          {
-                            key: 'delete',
-                            label: 'Delete Publication',
-                            icon: Delete,
-                            onPress: () => {
-                              deleteDialog.open(docId)
-                            },
-                          },
-                        ]}
-                      />
-                    </XStack>
-                  )
-                }}
-              />
-              {deleteDialog.content}
-            </YStack>
-          </MainWrapperNoScroll>
+                        )
+                      }
+                    }}
+                    publication={publication}
+                    author={author}
+                    editors={editors}
+                    menuItems={[
+                      copyLinkMenuItem(
+                        idToUrl(docId, undefined, publication.version),
+                        'Publication',
+                      ),
+                      {
+                        key: 'delete',
+                        label: 'Delete Publication',
+                        icon: Delete,
+                        onPress: () => {
+                          deleteDialog.open(docId)
+                        },
+                      },
+                    ]}
+                  />
+                )
+              })}
+            </Container>
+            {deleteDialog.content}
+          </MainWrapper>
           <Footer />
         </>
       )
